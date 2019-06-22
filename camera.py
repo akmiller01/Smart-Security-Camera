3a84d5a330a0a0ff7b24c84b6d3f47ece8db5fb9
Alex Miller <alex.k.miller@gmail.com>
2019-06-22 13:55:57 -0400

    Max area to try and stop false light-shift triggers

--- camera.py
+++ camera.py
@@ -21,16 +21,16 @@ class VideoCamera(object):
         self.contour_area = 0
         self.vs = PiVideoStream(resolution,framerate).start()
         time.sleep(self.conf["camera_warmup_time"])
-        
+
     def hflip(self,hflip=True):
         self.vs.hflip(hflip)
-        
+
     def vflip(self,vflip=True):
         self.vs.vflip(vflip)
-        
+
     def rotation(self,angle=0):
         self.vs.rotation(angle)
-        
+
     def exposure_mode(self,exposure_mode="auto"):
         self.vs.exposure_mode(exposure_mode)
 
@@ -70,34 +70,34 @@ class VideoCamera(object):
         timestamp = self.lt.now()
         ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
         found_obj = False
-        
+
         frame = imutils.resize(frame,width=500)
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray,(21,21),0)
-        
+
         if self.avg is None or self.avg_count < self.conf["camera_adjustment_frames"]:
             self.avg = gray.copy().astype("float")
             self.avg_count += 1
             if self.avg_count == self.conf["camera_adjustment_frames"]:
                 print("[INFO] motion detector live...")
             return (None, False)
-        
+
         cv2.accumulateWeighted(gray,self.avg,0.5)
         frameDelta = cv2.absdiff(gray,cv2.convertScaleAbs(self.avg))
-        
+
         # threshold the delta image, dilate the thresholded image to fill
         # in holes, then find contours on thresholded image
         thresh = cv2.threshold(frameDelta, self.conf["delta_thresh"], 255,cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-        
+
         # loop over the contours
         for c in cnts:
             # if the contour is too small, ignore it
             ca = cv2.contourArea(c)
             self.contour_area = ca
-            if ca < self.conf["min_area"]:
+            if ca < self.conf["min_area"] or ca > self.conf["max_area"]:
                 continue
 
             # compute the bounding box for the contour, draw it on the frame,
@@ -105,7 +105,7 @@ class VideoCamera(object):
             (self.x, self.y, self.w, self.h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (self.x, self.y), (self.x + self.w, self.y + self.h), (0, 255, 0), 2)
             found_obj = True
-        
+
         # check to see if the room is occupied
         if found_obj:
             print("[INFO] found object!")
@@ -121,7 +121,7 @@ class VideoCamera(object):
                 self.motionCounter = 0
                 vis = np.concatenate(self.motion_frames,axis=0)
                 return (vis, found_obj)
-            
+
             return (None, False)
 
         # otherwise, the room is not occupied
@@ -131,5 +131,3 @@ class VideoCamera(object):
             self.motionCounter = 0
             self.motion_frames = []
             return (None, False)
-
-
